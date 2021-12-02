54d40376eb8fe41b189dca430ec387d4fe00f83f
SuperH0ng <ghdtmdgus100@gmail.com>
2021-12-02 15:02:01 +0900

    simplify code

--- upgradeGugudan/score.py
+++ upgradeGugudan/score.py
@@ -1,10 +1,6 @@
 class Score :
     def __init__(self):
         self.presentScore = 0
-    
-    # def presentScore(self) :
-    #     return self.presentScore
 
     def correct(self, score) :
         return score + 1
-        # return score + 1
